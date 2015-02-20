621aaa73e8a4007725e68df8007f714e08faa0a5
CaptTofu <patg@patg.net>
2015-02-19 21:59:53 -0500

    Working changes (work in progress)

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python
+
 # chardet's setup.py
 from distutils.core import setup
 setup(
-    name = "comware_5_2",
+    name = "pro_vision",
     packages = [""],
     version = "0.2.0",
-    description = "Ansible Comware Library",
+    description = "Ansible Pro Vision Library",
     author = "Patrick Galbraith",
     author_email = "patg@hp.com",
     url = "http://patg.net/",
     download_url = "http://tbd.tgz",
-    keywords = ["comware", "hp switches", "ansible"],
+    keywords = ["pro-vision", "hp switches", "ansible"],
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
@@ -23,10 +25,10 @@ setup(
         ],
     long_description = """\
 
-Comware Switch Library for Ansible
+Provision Switch Library for Ansible
 -------------------------------------
 
-Contains routines to talk manage a Comware 5.2-based switch using Ansible
+Contains routines to talk manage a Pro Vision-based switch using Ansible
 
 This version requires Python 2.7 or later
 """
